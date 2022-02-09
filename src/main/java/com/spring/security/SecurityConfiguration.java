899f6414d8b59d11e3f7f17f7050c8aab41be33f
Hassan Askar <hassanemad15697@gmail.com>
2022-02-09 15:17:50 +0100

    security configuration in memory authontication

---
+++ src/main/java/com/spring/security/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.spring.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().
+                withUser("amro").password(passwordEncoder().encode("Amro123#")).roles("ADMIN").
+                and().
+                withUser("israa").password(passwordEncoder().encode("Israa123#")).roles("USER").
+                and().
+                withUser("hassan").password(passwordEncoder().encode("Hassan123#")).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
+    }
+@Bean
+    PasswordEncoder passwordEncoder ()
+    {
+        return new BCryptPasswordEncoder();
+    }
+}
