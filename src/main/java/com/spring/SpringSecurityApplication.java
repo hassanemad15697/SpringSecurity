03bade33c92f497c3da61f48add3334a466ce062
Hassan Askar <hassanemad15697@gmail.com>
2022-02-10 12:32:15 +0100

    convert to https and genereate ssl certificate

--- src/main/java/com/spring/SpringSecurityApplication.java
+++ src/main/java/com/spring/SpringSecurityApplication.java
@@ -1,7 +1,15 @@
 package com.spring;
 
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -9,5 +17,47 @@ public class SpringSecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
+    /*
+    to generate ssl certificate
+    1. go to C:\Program Files\Java\jdk1.8.0_111\bin
+    2. rum CMD as admin
+    3. write this command
+    .\keytool -genkey -alias bootsecurity -storetype PKCS12 -keyalg RSA -keysize 2084 -keystore bootsecurity.p12 -validity 3650
+    4.follow steps to configuration
+    DONE
+     */
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        // Enable SSL Trafic
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
 
+        // Add HTTP to HTTPS redirect
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+    }
+
+    /*
+    We need to redirect from HTTP to HTTPS. Without SSL, this application used
+    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
+    redirected to HTTPS on 8443.
+     */
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
 }
