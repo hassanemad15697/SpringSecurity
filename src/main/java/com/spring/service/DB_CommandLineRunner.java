e4bf279007b1efccff34c617984b6b01b765e1d1
Hassan Askar <hassanemad15697@gmail.com>
2022-02-10 15:59:18 +0100

    INSERT manager and empolyee and users  in database with CommandLineRunner

--- src/main/java/com/spring/service/DB_CommandLineRunner.java
+++ src/main/java/com/spring/service/DB_CommandLineRunner.java
@@ -11,6 +11,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -35,21 +37,43 @@ public class DB_CommandLineRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
 
-        User user= new User("Hassan Askar","hassan","Hassan123#","via ponale",1);
+       // User admin= new User("Hassan Askar","hassan","Hassan123#","via ponale",1);
+      //  admin.setRoles(this.roleDAO.findAll());
+     //   admin.setAuthorities(this.authorityDAO.findAll());
+    //    this.userDAO.save(admin);
 
+//        User manager= new User("Amro Askar","amro","Amro123#","sharkia",1);
+//        List<Role> managerRole = new ArrayList<>();
+//        managerRole.add(this.roleDAO.findById(2L).get());
+//        managerRole.add(this.roleDAO.findById(3L).get());
+//        managerRole.add(this.roleDAO.findById(4L).get());
+//        manager.setRoles(managerRole);
+//        List<Authority> authorities = new ArrayList<>();
+//        authorities.add(this.authorityDAO.findById(2L).get());
+//        authorities.add(this.authorityDAO.findById(3L).get());
+//        manager.setAuthorities(authorities);
+//        this.userDAO.save(manager);
 
+        User empolyee= new User("Israa Emad","israa","Israa123#","milano",1);
+        List<Role> empolyeeRole = new ArrayList<>();
+        empolyeeRole.add(this.roleDAO.findById(3L).get());
+        empolyeeRole.add(this.roleDAO.findById(4L).get());
+        empolyee.setRoles(empolyeeRole);
+        List<Authority> empolyeeAuthorities = new ArrayList<>();
+        empolyeeAuthorities.add(this.authorityDAO.findById(2L).get());
+        empolyeeAuthorities.add(this.authorityDAO.findById(3L).get());
+        empolyee.setAuthorities(empolyeeAuthorities);
+        this.userDAO.save(empolyee);
 
-
-        user.setRoles(this.roleDAO.findAll());
-        user.setAuthorities(this.authorityDAO.findAll());
-        for (Role role:user.getRoles()) {
-            System.out.println(role.getRoleName());
-        }
-
-        for (Authority authority: user.getAuthorities()) {
-            System.out.println(authority.getAuthorityName());
-        }
-
+        User user= new User("Hager Emad","hager","Hager123#","milano",1);
+        List<Role> userRole = new ArrayList<>();
+        userRole.add(this.roleDAO.findById(4L).get());
+        user.setRoles(userRole);
+        List<Authority> userAuthorities = new ArrayList<>();
+        userAuthorities.add(this.authorityDAO.findById(3L).get());
+        user.setAuthorities(userAuthorities);
         this.userDAO.save(user);
+
+
     }
 }
