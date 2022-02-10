d3050b94bbd32b6ecbed8f9680a715f706c60576
Hassan Askar <hassanemad15697@gmail.com>
2022-02-10 14:11:35 +0100

    add DAO Layer

---
+++ src/main/java/com/spring/dao/UserDAO.java
@@ -0,0 +1,9 @@
+package com.spring.dao;
+
+import com.spring.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDAO extends JpaRepository<User, Long> {
+}
